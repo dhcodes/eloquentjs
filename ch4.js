fc4b2a2ca6c44249fef561815db4af6798181e46
Dylan <thor.dh@gmail.com>
2017-02-25 15:08:17 -0600

    working on ch. 4

--- ch4.js
+++ ch4.js
@@ -1,5 +1,7 @@
 //Exercises
 
+console.log('#1. The sum of a range')
+
 /*
 The sum of a range
 
@@ -47,4 +49,100 @@ let sum = function(arr) {
 
 console.log(sum(range(1, 10)));
 
-let
+console.log('\n#2. A list')
+/*
+A list
+
+Objects, as generic blobs of values, can be used to build all sorts of data structures. A common data structure is the list (not to be confused with the array). A list is a nested set of objects, with the first object holding a reference to the second, the second to the third, and so on.
+
+var list = {
+  value: 1,
+  rest: {
+    value: 2,
+    rest: {
+      value: 3,
+      rest: null
+    }
+  }
+};
+
+A linked list
+A nice thing about lists is that they can share parts of their structure. For example, if I create two new values {value: 0, rest: list} and {value: -1, rest: list} (with list referring to the variable defined earlier), they are both independent lists, but they share the structure that makes up their last three elements. In addition, the original list is also still a valid three-element list.
+
+Write a function arrayToList that builds up a data structure like the previous one when given [1, 2, 3] as argument, and write a listToArray function that produces an array from a list. Also write the helper functions prepend, which takes an element and a list and creates a new list that adds the element to the front of the input list, and nth, which takes a list and a number and returns the element at the given position in the list, or undefined when there is no such element.
+
+If you haven’t already, also write a recursive version of nth.
+
+// Your code here.
+
+console.log(arrayToList([10, 20]));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(listToArray(arrayToList([10, 20, 30])));
+// → [10, 20, 30]
+console.log(prepend(10, prepend(20, null)));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(nth(arrayToList([10, 20, 30]), 1));
+// → 20
+*/
+
+function arrayToList(arr) {
+    let list = null;
+    for (let i = arr.length-1; i>=0; i--) {
+        list = {value: arr[i], rest: list}
+    }
+    return list;
+}
+
+console.log(arrayToList([10, 20, 30]))
+
+console.log('\n#3. Deep comparison')
+/*
+Deep comparison
+
+The == operator compares objects by identity. But sometimes, you would prefer to compare the values of their actual properties.
+
+Write a function, deepEqual, that takes two values and returns true only if they are the same value or are objects with the same properties whose values are also equal when compared with a recursive call to deepEqual.
+
+To find out whether to compare two things by identity (use the === operator for that) or by looking at their properties, you can use the typeof operator. If it produces "object" for both values, you should do a deep comparison. But you have to take one silly exception into account: by a historical accident, typeof null also produces "object".
+
+// Your code here.
+
+var obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(obj, obj));
+// → true
+console.log(deepEqual(obj, {here: 1, object: 2}));
+// → false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// → true
+*/
+
+
+function deepEqual(x,y) {
+    if (x === y && typeof x !== 'object') {
+        return true;
+    }
+    else {
+    if (typeof x === 'object' && x !== null) {
+    if (Object.keys(x).length !== Object.keys(y).length) {
+        return false;
+    }
+   
+
+        for (prop in x) {
+         
+                return deepEqual(x.prop, y.prop)
+            
+        }
+    }
+    
+}
+}
+
+var obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(obj, obj));
+// → true
+console.log(deepEqual(obj, {here: 1, object: 2}));
+// → false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// → true
+
